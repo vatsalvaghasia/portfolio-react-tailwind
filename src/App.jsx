b1105ac12cc5caf7ac72ae49f8fd9e105a6558d9
Vatsal Vaghasia <vaghasia84@gmail.com>
2025-06-09 15:29:47 -0500

    Setup github paged to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home";
 import { NotFound } from "./pages/NotFound";
 function App() {
     return (
         <>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route index element={<Home />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </>
     );
 }
