7b7688ff3541ff1d0c5d8350e83e1c4be9f52274
Vatsal Vaghasia <vaghasia84@gmail.com>
2025-06-09 15:51:31 -0500

    Setup github paged to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home";
-import { NotFound } from "./pages/NotFound";
+import { Home } from "@/pages/home";
+import { NotFound } from "@/pages/NotFound";
 function App() {
     return (
         <>
